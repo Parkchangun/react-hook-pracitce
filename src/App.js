ce005512ceadc997a287e80c9ee55f875b876993
Parkchangun <qkrcksrjs@gmail.com>
2022-01-10 18:51:54 +0900

    create useInput

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {useInput} from "./hooks";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const maxLength = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLength);
+
+  return (<div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo"/>
+      <input placeholder={"Name"} {...name}/>
+    </header>
+  </div>);
 }
 
 export default App;
